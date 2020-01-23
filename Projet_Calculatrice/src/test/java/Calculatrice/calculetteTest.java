7d676660c0a3eb3995e9842ff9c4e4e0303fcd22
Clement Finck <Clement Finck@DESKTOP-C6SHO8E>
2020-01-23 14:09:53 +0100

    Création des tests

--- Projet_Calculatrice/src/test/java/Calculatrice/calculetteTest.java
+++ Projet_Calculatrice/src/test/java/Calculatrice/calculetteTest.java
@@ -1,14 +1,51 @@
 package Calculatrice;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
+
+import CalculatriceSimple.test;
 
 public class calculetteTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	calculette macalculette;
+	@BeforeEach
+	void ini() {
+		macalculette = new calculette();
 	}
-
+	
+	@Test
+	@EnabledOnJre(JRE.JAVA_15)
+	
+	void testaddition() 
+	{
+		double expected = 0;
+		double actual = macalculette.addition(3,1);
+		assertEquals(expected, actual,"Opération d'addition");
+	}
+	
+	void testsoustraction() 
+	{
+		double expected = 4;
+		double actual = macalculette.soustraction(3,1);
+		assertEquals(expected, actual,"Opération d'addition");
+	}
+	
+	void testmultiplication() 
+	{
+		double expected = 4;
+		double actual = macalculette.multiplication(3,1);
+		assertEquals(expected, actual,"Opération d'addition");
+	}
+	
+	void testdivision() 
+	{
+		double expected = 4;
+		double actual = macalculette.division(3,1);
+		assertEquals(expected, actual,"Opération d'addition");
+	}
 }
